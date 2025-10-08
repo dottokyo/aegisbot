01f2e4872bc5ee5ca38f15b24e6c277b462ea41c
dottokyo <envelopeaddiction@gmail.com>
2025-10-08 05:15:02 +0300

    initialize modules

--- backend/app/main.py
+++ backend/app/main.py
@@ -44,9 +44,11 @@ async def get_trader_data():
     return trader_data
 
 if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=5001,
-        reload=True
+        port=port,
+        reload=False
     )
